09e979ea2d47a24c41f3c8ed88cac1eb0c7722b5
Arjun Bisen <arb502@student.hks.harvard.edu>
2018-01-08 17:31:59 -0500

    calculator to multiply 5 and 6

--- calculator.py
+++ calculator.py
@@ -2,18 +2,18 @@
 """this program defines four functions (multiply, add, subtract, and divide)"""
 # This part of the code defines a multiply function
 def multiply(a,b):
-	print "I'm multiplying two numbers"
 	return  a * b
 
+
 def add(a,b):
-	print "I'm adding two numbers"
 	return  a + b
 
 def subtract(a,b):
-	print "I'm subtracting two numbers"
 	return  a - b
 
 def divide(a,b):
-	print "I'm dividing one number by another"
 	return  a / b 
 
+print "I'm going use the calculator functions to multiply 5 and 6"
+x = multiply(5,6)
+print x
